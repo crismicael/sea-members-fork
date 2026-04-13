a103885ddb302a52fd95e907f2c8da96c3dd15f6
Seth García <sethgarciarivera@hotmail.com>
2026-04-13 11:21:52 -0600

    added hilton, ciao pasta and chinelo promos to carousel

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -179,6 +179,30 @@ export const lang = {
           'cta': 'Lee la historia de Solares',
           'href': ''
         },
+        '16': {
+          'brand': 'Hilton',
+          'description': 'DAY PASS PRECIO ESPECIAL: Adultos - 1440MXN, Adolecentes - 1080MXN, Niños 900MXN, INCLUYE 50% EN EFOREA SPA, ALIMENTOS, BEBIDAS Y ENTRETENIMIENTO.',
+          'cta': 'Lee la historia de Hilton',
+          'href': ''
+        },
+        '17': {
+          'brand': 'Hilton',
+          'description': '35% OFF POR NOCHE PARA RESIDENTES',
+          'cta': 'Lee la historia de Hilton',
+          'href': ''
+        },
+        '18': {
+          'brand': 'El Chinelo',
+          'description': '10% de descuento en alimentos y bebidas. Cupón disponible nuevamente al mes de uso.',
+          'cta': 'Lee la historia de El Chinelo',
+          'href': ''
+        },
+        '19': {
+          'brand': 'Ciao Pasta',
+          'description': 'Combo MEMBERS: $190 MXN Pasta o Pizza + 1 Refresco. Cupón disponible nuevamente al mes de uso.',
+          'cta': 'Lee la historia de Ciao Pasta',
+          'href': ''
+        },
       }
     },
     'qna': {
@@ -793,6 +817,30 @@ export const lang = {
           'brand': 'Solares',
           'description': '15% discount on solar panels.',
           'cta': 'Read the story of Solares',
+          'href': ''
+        },
+        '16': {
+          'brand': 'Hilton',
+          'description': 'DAY PASS SPECIAL PRICE: Adults - 1,440 MXN, Teens - 1,080 MXN, Children - 900 MXN. INCLUDES 50% OFF AT EFOREA SPA, FOOD, DRINKS, AND ENTERTAINMENT.',
+          'cta': 'Ready the story of Hilton',
+          'href': ''
+        },
+        '17': {
+          'brand': 'Hilton',
+          'description': '35% OFF PER NIGHT FOR RESIDENTS',
+          'cta': 'Ready the story of Hilton',
+          'href': ''
+        },
+        '18': {
+          'brand': 'El Chinelo',
+          'description': '10% off food and drinks. Discount available again one month after use.',
+          'cta': 'Ready the story of El Chinelo',
+          'href': ''
+        },
+        '19': {
+          'brand': 'Ciao Pasta',
+          'description': 'MEMBERS Combo: $190 MXN (Pasta or Pizza + 1 Soft Drink). Discount available again one month after use.',
+          'cta': 'Ready the story of Ciao Pasta',
           'href': ''
         },
       }
